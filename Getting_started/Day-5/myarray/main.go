8be9ae007fc0b2014d16bf323b09ef6911b44cdc
pradipbabar <pradipbaba13@gmail.com>
2023-10-19 16:42:07 +0530

    Day-5

---
+++ Getting_started/Day-5/myarray/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Array in Go")
+
+	var fruitList [4]string
+		fruitList[0] = "Apple"
+		fruitList[1] = "mango"
+		fruitList[2] = "peach"
+
+	fmt.Println("Fruit list is ", fruitList)
+	fmt.Println("Fruit list is ", len(fruitList))
+
+
+    var a [5]int
+    fmt.Println("emp:", a)
+
+    a[4] = 100
+    fmt.Println("set:", a)
+    fmt.Println("get:", a[4])
+
+    fmt.Println("len:", len(a))
+
+    b := [5]int{1, 2, 3, 4, 5}
+    fmt.Println("dcl:", b)
+
+    var twoD [2][3]int
+    for i := 0; i < 2; i++ {
+        for j := 0; j < 3; j++ {
+            twoD[i][j] = i + j
+        }
+    }
+    fmt.Println("2d: ", twoD)
+
+
+}
---
+++ Getting_started/Day-5/myslices/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	fmt.Println("Slices in Go")
+
+	var fruitList = []string{"apple","mango","banana"}
+	fmt.Printf("Type of fruitList is %T\n",fruitList)
+
+	fruitList = append(fruitList,"peach","orange")
+	fmt.Println(fruitList)
+
+	fruitList = append(fruitList[1:3])
+	fmt.Println(fruitList)
+
+	highScores := make([]int, 4)
+
+	highScores[0] = 234
+	highScores[1] = 945
+	highScores[2] = 465
+	highScores[3] = 867
+	//highScores[4] = 777
+
+	highScores = append(highScores, 555, 666, 321)
+
+	fmt.Println(highScores)
+
+	//fmt.Println(sort.IntsAreSorted(highScores))
+	sort.Ints(highScores)
+	fmt.Println(highScores)
+
+	//how to remove a value from slices based on index
+
+	var courses = []string{"reactjs", "javascript", "swift", "python", "ruby"}
+	fmt.Println(courses)
+	var index int = 2
+	courses = append(courses[:index], courses[index+1:]...)
+	fmt.Println(courses)
+
+
+
+}
