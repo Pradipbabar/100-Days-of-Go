6cbbb35cb6da6b2e94b8b0270de7e8ba7b61049e
pradipbabar <pradipbaba13@gmail.com>
2023-11-01 13:14:29 +0530

    Day-18

--- Advanced_Features/Day-18/webserv/main.go
+++ Advanced_Features/Day-18/webserv/main.go
@@ -4,9 +4,13 @@ import (
 	"fmt"
 	"net/http"
 	"io/ioutil"
+	"log"
+	"net"
+  
 )
 
-const url = "https://raw.githubusercontent.com/Pradipbabar/100-Days-of-Go/main/Advanced_Features/README.md"
+const url = "https://github.com/Pradipbabar/100-Days-of-Go"
+
 
 func main()  {
 	fmt.Println("Hello")
@@ -17,7 +21,7 @@ func main()  {
 		panic(err)
 	}
 
-	fmt.Println("Response",response)
+	// fmt.Println("Response",response)
 
 	defer response.Body.Close()
 
@@ -27,5 +31,29 @@ func main()  {
 		panic(err)
 	}
 	containt := string(databytes)
-	fmt.Println(containt)
+	// fmt.Println(containt)
+
+	http.HandleFunc("/", func (w http.ResponseWriter, 
+        r *http.Request) {
+  
+        // when receive the request, print the greeting meassage
+        fmt.Fprint(w, containt)
+  
+      })
+
+  // print out the server is going to start and show the time
+  log.Println("Starting server....")
+
+  // create server at localhost:8080 and using tcp as the network
+  listener, err := net.Listen("tcp", ":8080")
+
+  // if recieve error, record it and exit the program
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  // setup HTTP connection for the listener of the server
+  http.Serve(listener, nil)
+
+
 }
