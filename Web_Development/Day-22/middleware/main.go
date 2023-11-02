5768c6739387bbeffe423583cab164eb4f5061df
pradipbabar <pradipbaba13@gmail.com>
2023-11-02 13:44:04 +0530

    Day-22

--- Web_Development/Day-22/middleware/main.go
+++ Web_Development/Day-22/middleware/main.go
@@ -3,34 +3,26 @@ package main
 import (
     "fmt"
     "net/http"
-    "time"
-	"log"
 )
 
-//Create a request logging middleware handler called Logger
-type Logger struct {
-    handler http.Handler
-}
+func middleware(handler http.Handler) http.Handler {
+     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+         fmt.Println("Executing middleware before request phase!")
+         // Pass control back to the handler
+         handler.ServeHTTP(w, r)         
+		 fmt.Println("Executing middleware after response phase!")
+     })
+ }
 
-//ServeHTTP handles the request by passing it to the real handler and logging the request details
-func (l *Logger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-    start := time.Now()
-    l.handler.ServeHTTP(w, r)
-    log.Printf("%s %s %v", r.Method, r.URL.Path, time.Since(start))
-}
-
-
-func middlewareTimeHandler(w http.ResponseWriter, r *http.Request) {
-    curTime := time.Now().Format(time.Kitchen)
-    w.Write([]byte(fmt.Sprintf("the current time is %v", curTime)))
-}
+func mainLogic(w http.ResponseWriter, r *http.Request) {
+     // Business logic goes here
+     fmt.Println("Executing mainHandler...")
+     w.Write([]byte("OK")) 
+	 } 
 	 
 func main() {
-	addr := "localhost:8080"
-
-	 mux := http.NewServeMux()
-	 mux.HandleFunc("/", middlewareTimeHandler)
-	log.Printf("server is listening at %s", addr)
-    log.Fatal(http.ListenAndServe(addr, mux))
-
+     // HandlerFunc returns a HTTP Handler
+     mainLogicHandler := http.HandlerFunc(mainLogic)
+     http.Handle("/", middleware(mainLogicHandler))
+     http.ListenAndServe(":8000", nil)
 }
