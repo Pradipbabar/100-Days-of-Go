e2ccb8ef4bafe6fd62c2ef0c1df2fbffcdbbd979
Pradip Babar <65601230+Pradipbabar@users.noreply.github.com>
2023-11-29 14:17:31 +0530

    Update listiamge.go

--- Framworks_and_Tools/Containerization/GoDocker/action/listiamge.go
+++ Framworks_and_Tools/Containerization/GoDocker/action/listiamge.go
@@ -1,1 +1,29 @@
 package action
+
+import (
+	"context"
+	"fmt"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+	
+)
+
+func ListImagePackages(client client.Client) error {
+	// List images
+	images, err := client.ImageList(context.Background(), types.ImageListOptions{})
+	if err != nil {
+		return err
+	}
+
+	// Print package names for each image
+	for _, image := range images {
+		for _, tag := range image.RepoTags {
+			fmt.Printf("Image: %s\n", tag)
+			// You can extract more information about the image if needed
+			// For example, image.Labels may contain additional metadata
+		}
+	}
+
+	return nil
+}
+
