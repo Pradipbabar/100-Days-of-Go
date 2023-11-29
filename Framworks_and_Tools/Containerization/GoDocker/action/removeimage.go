c53535250a4c191f4289dfa06cd2cccfdb4018d4
Pradip Babar <65601230+Pradipbabar@users.noreply.github.com>
2023-11-29 14:24:49 +0530

    Docker

--- Framworks_and_Tools/Containerization/GoDocker/action/removeimage.go
+++ Framworks_and_Tools/Containerization/GoDocker/action/removeimage.go
@@ -1,1 +1,20 @@
 package action
+import (
+	"context"
+	"github.com/docker/docker/client"
+	"log"
+)
+
+func RemoveImage(client client.Client, imageName string) error {
+	_, err := client.ImageRemove(context.Background(), imageName, types.ImageRemoveOptions{
+		Force:         true, // Use force to remove the image even if it is in use by containers
+		PruneChildren: true, // Prune children (untagged or dangling images) as well
+	})
+	if err != nil {
+		log.Printf("Unable to remove image %s: %s", imageName, err)
+		return err
+	}
+
+	log.Printf("Image %s removed successfully", imageName)
+	return nil
+}
