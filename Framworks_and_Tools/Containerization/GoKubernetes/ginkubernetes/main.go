d146b505a509b0f57a14940c01bd7c58afffc1c7
Pradip Babar <65601230+Pradipbabar@users.noreply.github.com>
2023-11-30 07:50:03 +0000

    mod

--- Framworks_and_Tools/Containerization/GoKubernetes/ginkubernetes/main.go
+++ Framworks_and_Tools/Containerization/GoKubernetes/ginkubernetes/main.go
@@ -3,8 +3,9 @@ package main
 import (
 	"net/http"
 
+	"ginkubernetes/handlers"
+
 	"github.com/gin-gonic/gin"
-	"github.com/pradipbabar/ginkubernetes/handlers"
 )
 
 func SetupRoutes() *gin.Engine {
