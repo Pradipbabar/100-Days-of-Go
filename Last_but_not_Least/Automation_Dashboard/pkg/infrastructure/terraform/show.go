83c9ca14499e3d11112509fc8659da6313876b39
Pradip Babar <65601230+Pradipbabar@users.noreply.github.com>
2023-12-06 13:45:33 +0530

    Update show.go

--- Last_but_not_Least/Automation_Dashboard/pkg/infrastructure/terraform/show.go
+++ Last_but_not_Least/Automation_Dashboard/pkg/infrastructure/terraform/show.go
@@ -41,3 +41,67 @@ func ShowTerraform() error {
 	fmt.Println(state)
 	return nil
 }
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+// FlattenJSON flattens a nested JSON structure into a key-value map.
+func FlattenJSON(data map[string]interface{}, parentKey string, flattened map[string]interface{}) {
+	for key, value := range data {
+		newKey := key
+		if parentKey != "" {
+			newKey = fmt.Sprintf("%s.%s", parentKey, key)
+		}
+
+		switch value := value.(type) {
+		case map[string]interface{}:
+			FlattenJSON(value, newKey, flattened)
+		default:
+			flattened[newKey] = value
+		}
+	}
+}
+
+func main() {
+	// Example Terraform state JSON data (replace this with your actual data)
+	terraformStateJSON := `
+	{
+		"version": 4,
+		"terraform_version": "1.0.6",
+		"resources": [
+			{
+				"name": "example_resource",
+				"type": "aws_instance",
+				"instances": [
+					{
+						"index_key": "index_value",
+						"attributes": {
+							"id": "i-1234567890abcdef0",
+							"instance_type": "t2.micro"
+						}
+					}
+				]
+			}
+		]
+	}
+	`
+
+	var terraformState map[string]interface{}
+	err := json.Unmarshal([]byte(terraformStateJSON), &terraformState)
+	if err != nil {
+		fmt.Println("Error unmarshalling JSON:", err)
+		return
+	}
+
+	// Flatten Terraform state into key-value pairs
+	flattenedState := make(map[string]interface{})
+	FlattenJSON(terraformState, "", flattenedState)
+
+	// Print the flattened key-value pairs
+	for key, value := range flattenedState {
+		fmt.Printf("%s: %v\n", key, value)
+	}
+}
