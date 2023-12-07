6aec8f9815938e4101c5174a5e79d08b9688bc42
Pradip Babar <65601230+Pradipbabar@users.noreply.github.com>
2023-12-07 10:43:11 +0530

    Docker

--- Last_but_not_Least/Automation_Dashboard/cmd/container/show.go
+++ Last_but_not_Least/Automation_Dashboard/cmd/container/show.go
@@ -6,13 +6,29 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var (
+	showAllFlag bool
+	containerFlag bool
+	imageFlag bool
+)
+
 var showCmd = &cobra.Command{
 	Use:   "show",
 	Short: "show Container",
 	Long:  "show Container for your project.",
 	Run: func(cmd *cobra.Command, args []string) {
-
-		fmt.Println("showing Running Container...")
+// Check the values of bool flags and execute logic accordingly
+		if showAllFlag {
+			fmt.Println("Showing details for all containers...")
+		}
+		if containerFlag {
+			fmt.Printf("Showing details for container: %s\n", containerNameFlag)
+		}
+		if imageFlag {
+			fmt.Printf("Showing details for image: %s\n", imageNameFlag)
+		} else {
+			fmt.Println ()
+		}
 		// Implement initialization logic here
 
 	},
