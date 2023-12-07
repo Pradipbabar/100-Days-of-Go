c05d2b83b96cf023233dc22e68af710abd9deedd
Pradip Babar <65601230+Pradipbabar@users.noreply.github.com>
2023-12-07 10:47:29 +0530

    Docker

--- Last_but_not_Least/Automation_Dashboard/cmd/container/delete.go
+++ Last_but_not_Least/Automation_Dashboard/cmd/container/delete.go
@@ -6,14 +6,35 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var (
+	imageNameFlag     string
+	containerNameFlag string
+)
+
 var deleteCmd = &cobra.Command{
 	Use:   "delete",
 	Short: "delete Container",
 	Long:  "delete Container for your project.",
 	Run: func(cmd *cobra.Command, args []string) {
+       // Use the flag values in your logic
+		if imageNameFlag != "" {
+			fmt.Printf("Deleting container with image name: %s\n", imageNameFlag)
+		} else {
+			fmt.Println("Please provide an image name to delete a container.")
+		}
 
+		if containerNameFlag != "" {
+			fmt.Printf("Deleting container with name: %s\n", containerNameFlag)
+		} else {
+			fmt.Println("Please provide a container name to delete.")
+		}
 		fmt.Println("deleting Container...")
 		// Implement initialization logic here
 
 	},
+}
+func init() {
+	// Add flags to the delete command
+	deleteCmd.Flags().StringVarP(&imageNameFlag, "image", "I", "", "Specify image name for deletion")
+	deleteCmd.Flags().StringVarP(&containerNameFlag, "container", "c", "", "Specify container name for deletion")
 }
