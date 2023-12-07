02909dc344d8b3a2cef1af21a1c45d20fac123aa
Pradip Babar <65601230+Pradipbabar@users.noreply.github.com>
2023-12-07 11:50:40 +0530

    docker

--- Last_but_not_Least/Automation_Dashboard/pkg/container/docker/delete.go
+++ Last_but_not_Least/Automation_Dashboard/pkg/container/docker/delete.go
@@ -1,1 +1,71 @@
+// docker/docker.go
 package docker
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+)
+
+func DeleteImage(imageName string) {
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		panic(err)
+	}
+	defer cli.Close()
+
+	images, err := cli.ImageList(ctx, types.ImageListOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, image := range images {
+		for _, tag := range image.RepoTags {
+			// Check if the image name matches the provided imageName
+			if imageName == tag {
+				fmt.Print("Deleting image ", tag, "... ")
+				_, err := cli.ImageRemove(ctx, image.ID, types.ImageRemoveOptions{})
+				if err != nil {
+					panic(err)
+				}
+				fmt.Println("Success")
+				return // Exit the loop after deleting the image
+			}
+		}
+	}
+
+	// If the loop completes without finding the image, print a message
+	fmt.Println("Image not found:", imageName)
+}
+
+func DeleteContainer(containerName string) {
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		panic(err)
+	}
+	defer cli.Close()
+
+	containers, err := cli.ContainerList(ctx, types.ContainerListOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, container := range containers {
+		// Check if the container name matches the provided containerName
+		if containerName == container.Names[0][1:] {
+			fmt.Print("Deleting container ", container.ID[:10], "... ")
+			if err := cli.ContainerRemove(ctx, container.ID, types.ContainerRemoveOptions{}); err != nil {
+				panic(err)
+			}
+			fmt.Println("Success")
+			return // Exit the loop after deleting the container
+		}
+	}
+
+	// If the loop completes without finding the container, print a message
+	fmt.Println("Container not found:", containerName)
+}
